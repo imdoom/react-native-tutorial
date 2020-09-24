719c334288efd47e3877db37cd07b6a8d70db1fe
imdoom <akgp2707@yahoo.co.in>
2020-09-24 14:08:55 +0530

    Fetch data

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import CourseList from './components/CourseList';
 
@@ -29,17 +29,28 @@ const schedule = {
 };
 
 const Banner = ({title}) => (
-  <Text style={styles.bannerStyle}>{title}</Text>
+  <Text style={styles.bannerStyle}>{title || '[loading...]'}</Text>
 );
 
 const App = () => {
+  const [schedule, setSchedule] = useState({ title: '', courses: [] });
+  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php'
+  useEffect(() => {
+    const fetchSchedule = async () => {
+      const response = await fetch(url);
+      if (!response.ok) throw response;
+      const json = await response.json();
+      setSchedule(json);
+    };
+    fetchSchedule();
+  }, []);
   return (
     <SafeAreaView  style={styles.container}>
       <Banner title = {schedule.title}/>
       <CourseList courses={schedule.courses} />
     </SafeAreaView >
   );
-}
+};
 
 
 const styles = StyleSheet.create({
